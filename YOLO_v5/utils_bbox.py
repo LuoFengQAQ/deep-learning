541b45b14419bce5632ebaf20fb5b3e96d9d91f3
LuoFengQAQ <1122zzp@hrbeu.edu.cn>
2023-11-09 21:20:49 +0800

    Add files via upload

--- YOLO_v5/utils_bbox.py
+++ YOLO_v5/utils_bbox.py
@@ -1,4 +1,7 @@
 import torch
+import matplotlib.pyplot as plt
+import numpy as np
+from torchvision.ops import nms
 
 
 def get_anchors_and_decode(input, input_shape, anchors, anchors_mask, num_class):
@@ -36,4 +39,58 @@ def get_anchors_and_decode(input, input_shape, anchors, anchors_mask, num_class)
     prediction = input.view(batch_size, len(anchors_mask[2]),  # 特征层先验框的数量，理论上为3
                             num_class + 5, input_height, input_width).permute(0, 1, 3, 4, 2).contiguous()
 
-    x = torch
+    # ----------------------------------------------#
+    # 先验框中心位置参数调整
+    # ----------------------------------------------#
+    x = torch.sigmoid(prediction[..., 0])
+    y = torch.sigmoid(prediction[..., 1])
+    # ----------------------------------------------#
+    # 先验框宽高调整参数
+    # ----------------------------------------------#
+    w = torch.sigmoid(prediction[..., 2])
+    h = torch.sigmoid(prediction[..., 3])
+    # ----------------------------------------------#
+    # 获得置信度，是否有物体 0-1
+    # ----------------------------------------------#
+    conf = torch.sigmoid(prediction[..., 4])
+    # ----------------------------------------------#
+    # 种类置信度
+    # ----------------------------------------------#
+    pred_cls = torch.sigmoid(prediction[..., 5:])
+    FloatTensor = torch.cuda.FloatTensor if x.is_cuda else torch.FloatTensor
+    Longtensor = torch.cuda.LongTensor if x.is_cuda else torch.LongTensor
+    # ----------------------------------------------#
+    # 生成网络，先验框中心，网格左上角
+    # batch_size，3，20，20
+    # range(20)
+    # [
+    #  [0,1,2,...,19],
+    #  [0,1,2,...,19],
+    #  ...20列
+    #  [0,1,2,...,19]
+    #  batch_size,3,20,20
+    # ]
+    # ----------------------------------------------#
+    grid_x = torch.linspace(0, input_width - 1, input_width).repeat(input_height, 1).repeat(
+        batch_size * len(anchors_mask[2]), 1, 1).view(x.shape).type(FloatTensor)
+    grid_y = torch.linspace(0, input_height - 1, input_height).repeat(input_width, 1).repeat(
+        batch_size * len(anchors_mask[2]), 1, 1).view(y.shape).type(FloatTensor)
+    # ----------------------------------------------#
+    # 按照网格格式生成先验框的宽高
+    # batch_size,3,20,20
+    # ----------------------------------------------#
+
+    anchor_w = FloatTensor(scaled_anchors).index_select(1, Longtensor([0]))
+    anchor_h = FloatTensor(scaled_anchors).index_select(1, Longtensor([1]))
+    anchor_w = anchor_w.repeat(batch_size, 1).repeat(1, 1, input_height * input_width).view(w.shape)
+    anchor_h = anchor_h.repeat(batch_size, 1).repeat(1, 1, input_height * input_width).view(h.shape)
+    # ----------------------------------------------#
+    # 利用预测结果对先验框进行调整
+    # 首先调整先验框的中心，从先验框中心向右下角偏移
+    # 再调整先验框的宽高
+    # ----------------------------------------------#
+    pred_boxes = FloatTensor(prediction[..., :4].shape)
+    pred_boxes[..., 0] = x.data * 2 - 0.5 + grid_x
+    pred_boxes[..., 1] = y.data * 2 - 0.5 + grid_y
+    pred_boxes[..., 2] = (w.data * 2) ** 2 * anchor_w
+    pred_boxes[..., 3] = (h.data * 2) ** 2 * anchor_h
